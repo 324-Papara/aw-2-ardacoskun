f24503d75bcc76d8ab4c661b746882f05dc34476
Arda Coşkun <m.ardacoskun@gmail.com>
2024-07-18 15:55:58 +0300

    include and where methods added

--- Para.Api/Para.Data/GenericRepository/GenericRepository.cs
+++ Para.Api/Para.Data/GenericRepository/GenericRepository.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Para.Base.Entity;
 using Para.Data.Context;
+using System.Linq.Expressions;
 
 namespace Para.Data.GenericRepository;
 
@@ -18,9 +19,33 @@ public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEnt
         await dbContext.SaveChangesAsync();
     }
 
+    public async Task<List<TEntity>> GetAll()
+    {
+        return await dbContext.Set<TEntity>().ToListAsync();
+    }
+    public async Task<List<TEntity>> GetAllWithIncludeAsync(params Expression<Func<TEntity, object>>[] includes)
+    {
+        IQueryable<TEntity> query = dbContext.Set<TEntity>();
+        query = includes.Aggregate(query, (current, include) => current.Include(include));
+        return await query.ToListAsync();
+
+    }
     public async Task<TEntity> GetById(long Id)
     {
         return await dbContext.Set<TEntity>().FirstOrDefaultAsync(x => x.Id == Id);
+    }
+
+    public async Task<TEntity> GetWithIncludeByIdAsync(long Id, params Expression<Func<TEntity, object>>[] includes)
+    {
+        IQueryable<TEntity> query = dbContext.Set<TEntity>();
+        query = includes.Aggregate(query, (current, include) => current.Include(include));
+        return await query.FirstOrDefaultAsync(x => x.Id == Id);
+    }
+
+
+    public async Task<List<TEntity>> Where(Expression<Func<TEntity, bool>> condition)
+    {
+        return await dbContext.Set<TEntity>().Where(condition).ToListAsync();
     }
 
     public async Task Insert(TEntity entity)
@@ -45,10 +70,5 @@ public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEnt
     {
         var entity = await dbContext.Set<TEntity>().FirstOrDefaultAsync(x => x.Id == Id);
         dbContext.Set<TEntity>().Remove(entity);
-    }
-
-    public async Task<List<TEntity>> GetAll()
-    {
-       return await dbContext.Set<TEntity>().ToListAsync();
     }
 }
--- Para.Api/Para.Data/GenericRepository/IGenericRepository.cs
+++ Para.Api/Para.Data/GenericRepository/IGenericRepository.cs
@@ -1,12 +1,18 @@
+using System.Linq.Expressions;
+
 namespace Para.Data.GenericRepository;
 
 public interface IGenericRepository<TEntity> where TEntity : class
 {
     Task Save();
+    Task<List<TEntity>> GetAll();
+    Task<List<TEntity>> GetAllWithIncludeAsync(params Expression<Func<TEntity, object>>[] includes);
     Task<TEntity?> GetById(long Id);
+    Task<TEntity?> GetWithIncludeByIdAsync(long Id, params Expression<Func<TEntity, object>>[] includes);
+    Task<List<TEntity>> Where(Expression<Func<TEntity, bool>> predicate);
     Task Insert(TEntity entity);
     Task Update(TEntity entity);
     Task Delete(TEntity entity);
     Task Delete(long Id);
-    Task<List<TEntity>> GetAll();
+
 }
